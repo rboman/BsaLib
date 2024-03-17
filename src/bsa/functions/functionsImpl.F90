c1b2c74a935667090dec7311d9e231d8329be79c
micheleEsMar <michele.espositomarzino@uliege.be>
2024-03-17 08:23:39 +0100

    [IMPV] Remove superfluous offset indexes

--- src/bsa/functions/functionsImpl.F90
+++ src/bsa/functions/functionsImpl.F90
@@ -1324,7 +1324,6 @@ contains
       integer(int32) :: itc, tc, tc_posN, tc_pk, tc_pj, tc_pi
 
       ! nodes indexed values
-      integer(int32) :: i_pos_nk, i_pos_nj, i_pos_ni
       integer(int32) :: pos_nk, pos_nj, pos_ni
       integer(int32) :: ink, inj, ini
       integer(int32) :: ni, nj, nk
@@ -1428,24 +1427,22 @@ contains
          tcP3    = tc + 3   ! quadratic term coeff
          tc_posN = (itc - 1) * NNODESL
 
-         i_pos_nk = 1
          do ink = 1, NNODESL
 
             nk     = struct_data%n_load_(ink)
             pos_nk = (nk - 1) * NLIBS
-            tc_pk  = tc_posN + i_pos_nk
+            tc_pk  = tc_posN + ink
 
             phik_(:, 1) = wd%phi_times_A_ndegw_(:, ink, tc  )
             phik_(:, 2) = wd%phi_times_A_ndegw_(:, ink, tcP3)
 
             S_uvw_k = Suvw(:, tc_pk)
 
-            i_pos_nj = 1
             do inj = 1, NNODESL
 
                nj     = struct_data%n_load_(inj)
                pos_nj = (nj - 1) * NLIBS
-               tc_pj  = tc_posN + i_pos_nj
+               tc_pj  = tc_posN + inj
 
                phij_(:, 1) = wd%phi_times_A_ndegw_(:, inj, tc  )
                phij_(:, 2) = wd%phi_times_A_ndegw_(:, inj, tcP3)
@@ -1459,12 +1456,11 @@ contains
                !! BISPs
                if (settings%i_compute_bisp_ == 1) then
 
-                  i_pos_ni = 1
                   do ini = 1, NNODESL
 
                      ni     = struct_data%n_load_(ini)
                      pos_ni = (ni - 1) * NLIBS
-                     tc_pi  = tc_posN + i_pos_ni
+                     tc_pi  = tc_posN + ini
 
                      phii_(:, 1) = wd%phi_times_A_ndegw_(:, ini, tc  )
                      phii_(:, 2) = wd%phi_times_A_ndegw_(:, ini, tcP3)
@@ -1544,7 +1540,6 @@ contains
                      !$omp end parallel do
 #endif
 
-                     i_pos_ni = i_pos_ni + 1
                   enddo ! i node
 
                endif ! bisp computation
@@ -1564,15 +1559,12 @@ contains
                   enddo ! j mode
                enddo ! k mode
 
-               i_pos_nj = i_pos_nj + 1
             enddo ! j node
 
             i_cycle = i_cycle + 1
             print '(1x, a, " done ", f10.4, " %")', &
                INFOMSG, real(i_cycle, bsa_real_t)/real(NNODESL*NTCOMPS, bsa_real_t) * 100._bsa_real_t
-
-            i_pos_nk = i_pos_nk + 1
-         enddo ! k node
+        enddo ! k node
       enddo ! itc
 
 
