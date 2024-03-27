3652a9019f4390a3fa25a81a54c76bf228efc965
micheleEsMar <michele.espositomarzino@uliege.be>
2024-03-27 10:12:14 +0100

    [IMPV] Better error control

--- build.bat
+++ build.bat
@@ -4,20 +4,24 @@ setlocal
 if not exist "build_cmake" mkdir "build_cmake"
 
 set "cleanfirst=--clean-first"
-REM set "cleanfirst="
+set "cleanfirst="
 
 rem -D CMAKE_BUILD_TYPE=Debug ^
 
 cmake ^
    -D BUILD_SHARED_LIBS=OFF ^
-   -D enable-openmp=OFF ^
+   -D enable-openmp=ON ^
    -D enable-sym-ev-routine=OFF ^
    -D enable-single=OFF ^
    -D enable-gpu-code=OFF ^
    -D enable-cuda=ON ^
    -D enable-gpu-double=OFF ^
    -S . -B build_cmake %~1
+if not "%errorlevel%"=="0" exit /b 1
 
 cmake --build build_cmake --config Debug %cleanfirst%
+if not "%errorlevel%"=="0" exit /b 1
+
 cmake --build build_cmake --config Release %cleanfirst%
+if not "%errorlevel%"=="0" exit /b 1
 endlocal
